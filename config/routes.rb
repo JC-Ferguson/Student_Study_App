493f3bfde7a6a5ab61c6c43408131c2a501a5bc8
JC-Ferguson <jcferguson1996@gmail.com>
2019-10-29 19:15:29 +1100

    set up of routes, cpntrollers, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/", to: "pages#home", as: "root"
+  get "/users_connect", to: "users#index", as: "users"
+  post "/users_connect", to: "users#create"
+  get "/users_connect/new", to: "users#new", as: "new_user"
+  get "/users_connect/:id", to: "users#show", as: "user_profile"
+  patch "/user_connect/:id", to: "users#update"
+  get "/user_connect/:id/edit", to: "users#edit", as: "edit_users"
 end
